fd1929d5b196e5cc188026a85ef0f5014cabb9a8
Michael Herrmann <michael@herrmann.io>
2020-11-18 08:35:39 +0100

    Fix compilation error

--- chromium_src/components/component_updater/component_updater_service.cc
+++ chromium_src/components/component_updater/component_updater_service.cc
@@ -3,6 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "components/component_updater/component_updater_service.h"
+
 #define CheckForUpdates CheckForUpdates_ChromiumImpl
 #include "../../../../components/component_updater/component_updater_service.cc"
 #undef CheckForUpdates
