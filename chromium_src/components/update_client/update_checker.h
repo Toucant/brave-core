e58bdc2e4e935ae69e76f1dc61fddb9071796165
Michael Herrmann <michael@herrmann.io>
2020-11-20 19:39:25 +0100

    Fix further compilation failures

--- chromium_src/components/update_client/update_checker.h
+++ chromium_src/components/update_client/update_checker.h
@@ -1,6 +1,12 @@
 #ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_UPDATE_CLIENT_UPDATE_CHECKER_H_
 #define BRAVE_CHROMIUM_SRC_COMPONENTS_UPDATE_CLIENT_UPDATE_CHECKER_H_
 
+// 'Create' - defined below - is unfortunately a very common identifier.
+// Include those header files mentioned in update_checker.h which also end up
+// defining it. This way, the header files won't get their Create replaced.
+#include "base/callback.h"
+#include "components/update_client/component.h"
+
 #define Create    \
   Create_ChromiumImpl( \
       scoped_refptr<Configurator> config, \
