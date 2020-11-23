c4dd298277d7b36f735aa20c4d0e7f2182a06c54
Michael Herrmann <michael@herrmann.io>
2020-11-23 19:34:31 +0100

    Fix compilation failure

--- chromium_src/components/update_client/update_client.cc
+++ chromium_src/components/update_client/update_client.cc
@@ -3,6 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "components/update_client/update_client.h"
+
 #define UpdateClientFactory UpdateClientFactory_ChromiumImpl
 #include "../../../../components/update_client/update_client.cc"
 #undef UpdateClientFactory
